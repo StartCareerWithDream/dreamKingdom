3200097c4216b47237aa66837a4d70eaf24562c5
Yang Wangwang <m18270889015@163.com>
2017-08-29 09:51:40 +0800

    update

--- app/js/index.controller.js
+++ app/js/index.controller.js
@@ -1,9 +1,17 @@
 /**
  * Created by yangwangwang on 2017/8/28.
  */
-angular.module('app').config(['$stateProvider',function($stateProvider){
-   $stateProvider.state('login',{
-       url:'',
-       templateUrl:'./views/login/login.html'
-    });
+angular.module('app').config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+    /*路由重定向 $urlRouterProvider:如果没有路由引擎能匹配当前的导航状态，那它就会默认将路径路由至 home.html,
+     *这个页面就是状态名称被声明的地方. */
+    $urlRouterProvider.otherwise('/login');
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: './views/login/login.html'
+        })
+        .state('mySelect', {
+            url: '/mySelect',
+            template: '<my-select></my-select>'
+        })
 }]);
